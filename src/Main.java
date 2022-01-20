aeb8ec5f9761fe3845f999749d0a0cdbc4841fdc
Ilya Maximov <xc.navi@gmail.com>
2022-01-20 12:18:32 +0200

    Task 7.5.1 solution

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Task 7.5.1");
+
+
+        System.out.println("Calculation through interface:");
+        Calculatable c = new Calculation();
+        System.out.println(c.sum(1, 2));
+        System.out.println(c.sum(1, 2, 4));
+
+
     }
 
 
